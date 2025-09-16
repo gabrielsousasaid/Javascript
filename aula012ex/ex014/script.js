77ef424e5eab0cd830f322dda092037b45272437
Gabriel Said <72895033+gabrielsousasaid@users.noreply.github.com>
2025-09-16 13:38:50 -0300

    doing exercises

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,21 +1,29 @@
+
+//Criada função atralada ao evento de carregar página
 function carregar() {
 
-    const msg = document.getElementById('msg')
+    const msg = document.getElementById('msg')//Pegando os elementos a serem manipulados
     const img = document.getElementById('imagem')
     const data = new Date()
-    const hora = data.getHours()
+    //const hora = data.getHours()
+    const hora = 9
 
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 1 && hora <= 5) {
-        console.log('Boa madrugada')
-    } else if (hora >= 6 && hora <= 11) {
+     if (hora >= 1 && hora <= 11) {
         console.log('Bom dia')
+        img.src = 'foto-manha.png'
+        document.body.style.background = '#edddc2'
     } else if (hora >= 12 && hora <= 17) {
         console.log('Boa tarde')
+        img.src= 'foto-tarde.png'
+         document.body.style.background = '#fecea9'
     } else if (hora >= 18 && hora <= 23) {
         console.log('Boa noite')
+        img.src= 'foto-noite.png'
+        document.body.style.background = '#013f4e'
     } else {
         console.log('Você não inseriu um formato de hora válido')
-    }
+    }/* Script para mostrar a hora do dia e personalizar a página 
+    de acordo com o horário, toda vez que carregar a página */
 }
 
