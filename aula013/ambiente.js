6fa273317c9041f45c17c08608c87e7225428873
Gabriel Said <72895033+gabrielsousasaid@users.noreply.github.com>
2025-09-23 22:13:27 -0300

    Create ambiente.js

---
+++ aula013/ambiente.js
@@ -0,0 +1,24 @@
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+let c = 1
+
+while(c <=3){
+
+    console.log(`Passo ${c}`)
+    c++
+}
+
+let d = 1
+
+do{
+    console.log(`Passo ${d}`)
+    d++
+}while(d <= 3)
